0006a26f355cfc6f1d05a6aaf4f43069da9c8a61
antonio <antonio@AntonioCarlos>
2019-03-25 16:39:42 -0300

    Migrations

---
+++ database/migrations/2019_03_25_172009_create_eventos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->text('descricao');
+            $table->date('data_evento');
+            $table->timestamp('adicionado_em');
+            $table->integer('ativo');
+            $table->timestamps();
+            $table->integer('user_id')->UNSIGNED();
+            $table->foreign('user_id')
+            ->references('id')->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+}
